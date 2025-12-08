dbf6837bfb12e7da9ee7bc2c66c8498a015d5a7c
itsmmarc <officialmarcusmifsud@gmail.com>
2025-12-09 01:27:12 +1100

    refactor speedo bars to transform texture instead of using frames

---
+++ materials/vgui/replay/thumbnails/speedo/generator/generate_line_vertical.ps1
@@ -0,0 +1,26 @@
+# Note that this script is mostly included for those who know what they're doing. It cannot be run from here
+# ImageMagick must be installed in the same folder
+# The font must also be in the same folder (from my experience)
+# overengineered shell script by Whisker
+# See imagemagick documentation here if needed:
+# https://imagemagick.org/script/command-line-options.php#gravity
+
+# Define parameters
+$maxNumber = 1
+$backgroundColor = "Transparent"
+$VTFSize = "4x512"
+$width = 4
+$height = 512
+$heightB = $height - 1
+$end = $width - 1
+$folder = "line"
+$imagesFolder = "$PSScriptRoot\$folder"
+
+$bar = "rectangle 0,$height $end,$heightB"
+
+Write-Host "Starting script..."
+
+# Generate image
+magick.exe -size $VTFSize xc:$backgroundColor -fill white -draw $bar "$imagesFolder\line.png"
+
+Write-Host "Script completed."
